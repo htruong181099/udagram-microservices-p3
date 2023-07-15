c69ed4b4380fb59697f29d28d20a6ed97dfbd93f
hoangtruong181099 <hoangtruong181099@gmail.com>
2023-07-15 23:05:10 +0700

    add travis ci config

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_USERNAME=udagramhtm14
+export POSTGRES_PASSWORD=udagramhtm14
+export POSTGRES_HOST=udagram-htm14-micro.cgzhp6x2wmgc.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::udagram-micro-htm14-dev
+export AWS_BUCKET=udagram-micro-htm14-dev
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
