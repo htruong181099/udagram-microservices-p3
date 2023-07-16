60cc72da6776d60338c7ffe46ee4c5c3ec298ea0
hoangtruong181099 <hoangtruong181099@gmail.com>
2023-07-16 16:36:36 +0700

    refactor docker + fe url

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -3,9 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+	production: false,
+	appName: 'Udagram',
+	apiHost:
+		'http://a7109849934f342f19c2a7491d6e13da-1201745052.us-east-1.elb.amazonaws.com/api/v0',
 };
 
 /*
